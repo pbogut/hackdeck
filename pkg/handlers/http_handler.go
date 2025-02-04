f9b6f616e1340205c01b831d4f5800e40b9160db
Pawel Bogut <pbogut@pbogut.me>
2025-02-04 19:58:20 +0100

    refactor: split into packages

---
+++ pkg/handlers/http_handler.go
@@ -0,0 +1,29 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"os"
+
+	"github.com/pbogut/hackdeck/pkg/types"
+)
+
+// pingHandler handles the /ping route
+func PingHandler(w http.ResponseWriter, r *http.Request) {
+	// Get the hostname
+	hostname, err := os.Hostname()
+	if err != nil {
+		http.Error(w, "Unable to get hostname", http.StatusInternalServerError)
+		return
+	}
+	// Create the response object
+	response := types.PingResponse{
+		MachineName: hostname,
+	}
+	// Set the content type to application/json
+	w.Header().Set("Content-Type", "application/json")
+	// Encode the response as JSON and write it to the response writer
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		http.Error(w, "Error encoding JSON", http.StatusInternalServerError)
+	}
+}
---
+++ pkg/types/http.go
@@ -0,0 +1,5 @@
+package types
+
+type PingResponse struct {
+	MachineName string `json:"machineName"`
+}
